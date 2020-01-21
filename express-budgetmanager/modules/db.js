46a201c7cee19062cd67d9bd28f68551cfc15a88
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2020-01-21 13:11:11 +0100

    create read methodes in db.js

--- express-budgetmanager/modules/db.js
+++ express-budgetmanager/modules/db.js
@@ -2,8 +2,34 @@ const mariadb = require('mariadb');
 // npm i mariadb
 
 const pool = mariadb.createPool({
-    user: 'root',
-    password: 'root',
-    database: 'budgetmanager',
-    connectionLimit: 5,
+  user: 'root',
+  password: 'root',
+  database: 'budgetmanager',
+  connectionLimit: 5,
 });
+
+module.exports = class DB {
+  constructor() {
+    pool.getConnection().then((conn) => {
+      this.conn = conn;
+    });
+  }
+
+  async readCosts() {
+    const sql = `
+    SELECT * FROM costs
+    `;
+
+    const result = await this.conn.query(sql);
+    return result;
+  }
+
+  async readIncomes() {
+    const sql = `
+    SELECT * FROM incomes
+    `;
+
+    const result = await this.conn.query(sql);
+    return result;
+  }
+};
