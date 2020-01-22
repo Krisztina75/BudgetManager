fb4464d53539e87c64e4517e533921a91f7a583e
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2020-01-22 10:43:04 +0100

    create routing in app-routing.module.ts

--- angular-budgetmanager/src/app/app-routing.module.ts
+++ angular-budgetmanager/src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ReportsComponent } from './pages/reports/reports.component';
+import { SavingsComponent } from './pages/savings/savings.component';
+import { SummaryComponent } from './pages/summary/summary.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [{
+  path: 'summary',
+  component: SummaryComponent
+},
+{
+  path: 'reports',
+  component: ReportsComponent
+},
+{
+  path: 'savings',
+  component: SavingsComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-budgetmanager/src/app/app.module.ts
+++ angular-budgetmanager/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { SavingsComponent } from './pages/savings/savings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent,
+    ReportsComponent,
+    SavingsComponent
   ],
   imports: [
     BrowserModule,
