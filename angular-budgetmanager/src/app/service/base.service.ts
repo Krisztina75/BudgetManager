7ffb77aeddc8651310d1f459fa2888b88774b5d3
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2020-01-28 14:21:43 +0100

    create base service - getAll method

---
+++ angular-budgetmanager/src/app/service/base.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  baseUrl = 'http://localhost:3000/api';
+  entity: string;
+
+  constructor(protected http: HttpClient) { }
+
+  getUrl(): any {
+    let url = `${this.baseUrl}${this.entity}`;
+    return url;
+  }
+
+  getAll(): Observable<any> {
+    let url: string = this.getUrl();
+    return this.http.get(url);
+  }
+}
