9fc988625e42be45b8c5ce1b9258634759f99112
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2020-02-05 17:10:58 +0100

    amounts by categories are shown on  income reports page

--- angular-budgetmanager/src/app/pages/incomes-table/incomes-table.component.ts
+++ angular-budgetmanager/src/app/pages/incomes-table/incomes-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Income } from 'src/app/model/income';
 import { IncomesService } from 'src/app/service/incomes.service';
+import { Category } from 'src/app/model/category';
 
 @Component({
   selector: 'app-incomes-table',
@@ -9,11 +10,15 @@ import { IncomesService } from 'src/app/service/incomes.service';
 })
 export class IncomesTableComponent implements OnInit {
   allData: Income[] = [];
+  allCategories: Category[] = [];
 
-  constructor(private incomeService: IncomesService) { 
-    this.incomeService.getAll().subscribe(income => this.allData = income)
+  constructor(private incomeService: IncomesService) {
+    this.incomeService.getAll().subscribe(incomes => {
+      this.allData = incomes.allIncomeItem;
+      this.allCategories = incomes.categories;
+    })
   }
-
+  
   ngOnInit() {
   }
 
