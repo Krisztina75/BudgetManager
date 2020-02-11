b51ed2ce613c4c06d865db4c2b1f3e26182f688d
Krisztina Lajos-Kovács <kovacskrisztinamonika@gmail.com>
2020-02-11 15:54:21 +0100

    delete the empty my-doughnut-chart component

--- angular-budgetmanager/src/app/app.module.ts
+++ angular-budgetmanager/src/app/app.module.ts
@@ -13,7 +13,6 @@ import { SumPipe } from './pipe/sum.pipe';
 import { IncomeVsSpendingComponent } from './pages/income-vs-spending/income-vs-spending.component';
 import { SpendingByCategoryComponent } from './pages/spending-by-category/spending-by-category.component';
 import { ChartsModule } from 'ng2-charts';
-import { MyDoughnutChartComponent } from './pages/my-doughnut-chart/my-doughnut-chart.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +24,6 @@ import { MyDoughnutChartComponent } from './pages/my-doughnut-chart/my-doughnut-
     SumPipe,
     IncomeVsSpendingComponent,
     SpendingByCategoryComponent,
-    MyDoughnutChartComponent,
   ],
   imports: [
     BrowserModule,
